176c86af32078719ec79201432f63d0e7560dcc3
lvyao <lvyao.china@gmail.com>
2016-12-02 21:41:53 +0800

    To be continued ...

---
+++ app/api_view_funcs.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from flask import request, g
+
+from .api_utils import *
+
+
+__all__ = [
+    'list_objects',
+    'get_object',
+    'delete_object',
+    'delete_objects'
+]
+
+
+def list_objects(model, mark='objects'):
+    """
+    列出全部对象
+    :param model:
+    :param mark:
+    :return:
+    """
+    order_by, page, per_page = map(request.args.get, ('order_by', 'page', 'per_page'))
+    order_by = order_by.split(',') if order_by else None
+    claim_args_digits_string(1202, *filter(None, (page, per_page)))
+    data = {
+        mark: [obj.to_dict(g.fields) for obj in model.iterator(None, order_by, page, per_page)],
+        'total': model.count()
+    }
+    return api_success_response(data)
+
+
+def get_object(_id, model, mark='object'):
+    """
+    获取单个对象
+    :param _id:
+    :param model:
+    :param mark:
+    :return:
+    """
+    obj = model.query_by_id(_id)
+    claim_args(1104, obj)
+    data = {
+        mark: obj.to_dict(g.fields)
+    }
+    return api_success_response(data)
+
+
+def delete_object(_id, model):
+    """
+    删除单个对象
+    :param _id:
+    :param model:
+    :return:
+    """
+    obj = model.query_by_id(_id)
+    claim_args(1104, obj)
+    obj.delete_instance(recursive=True)
+    return api_success_response({})
+
+
+def delete_objects(model):
+    """
+    删除多个对象
+    :param model:
+    :return:
+    """
+    ids = request.args.get('ids')
+    claim_args(1601, ids)
+    ids = ids.split(',')
+    claim_args_digits_string(1602, *ids)
+    objs = map(model.query_by_id, ids)
+    claim_args(1104, *objs)
+    map(lambda o: o.delete_instance(recursive=True), objs)
+    return api_success_response({})
--- app/blueprints/www_main/extensions.py
+++ app/blueprints/www_main/extensions.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from flask import current_app, jsonify
+import hashlib
+
+from flask import current_app, request, make_response, jsonify
+import xmltodict
 
 from . import bp_www_main
 from utils.qiniu_util import get_upload_token
@@ -16,3 +19,36 @@ def get_qiniu_upload_token():
         'uptoken': get_upload_token(current_app.config['QINIU'])
     }
     return jsonify(data)
+
+
+@bp_www_main.route('/extensions/weixin/api/', methods=['GET', 'POST'])
+def weixin_api():
+    """
+    （由微信调用）微信API
+    :return:
+    """
+    signature, timestamp, nonce = map(request.args.get, ('signature', 'timestamp', 'nonce'))
+    if not all((signature, timestamp, nonce)):
+        current_app.logger.error(u'微信API验证参数不完整')
+        return make_response('')
+
+    items = [current_app.config['WEIXIN']['token'], timestamp, nonce]
+    items.sort()
+    hashcode = hashlib.sha1(''.join(items)).hexdigest()
+    if hashcode != signature:
+        current_app.logger.error(u'微信API验证失败')
+        return make_response('')
+
+    if request.method == 'GET':
+        current_app.logger.info(u'微信API验证成功')
+        return make_response(request.args.get('echostr', ''))
+
+    if request.method == 'POST':
+        xml = ''
+        try:
+            message = xmltodict.parse(request.data)['xml']
+            current_app.logger.info(message)
+        except Exception, e:
+            current_app.logger.error(e)
+        finally:
+            return make_response(xml)
