17e486617247a60419d7d0cb5ff500c2ecb03683
lvyao <lvyao.china@gmail.com>
2016-12-23 12:25:10 +0800

    To be continued ...

--- utils/pattern_util.py
+++ utils/pattern_util.py
@@ -25,6 +25,15 @@ def check_phone(phone):
     return bool(re.match(r'1[34578]\d{9}$', phone))
 
 
+def check_email(email):
+    """
+    检查电子邮箱格式是否正确
+    :param email:
+    :return:
+    """
+    return bool(re.match(r'\S+@\S+\.\S+$', email))
+
+
 def check_id_card(id_card):
     """
     检查身份证号码格式是否正确
